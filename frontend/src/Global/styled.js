4358cfca099cda07e1841c61387e2c53e53a39fc
Breno Felipe de Bairros <brendo.filipe2050@gmail.com>
2020-10-11 18:25:48 -0300

    Terminando a tela principal do aluno

--- frontend/src/Global/styled.js
+++ frontend/src/Global/styled.js
@@ -6,4 +6,16 @@ export default createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
   }
+
+  ::-webkit-scrollbar {
+    width: 10px;
+  }
+  ::-webkit-scrollbar-track {
+    background-color: #eeeeee;
+    border-radius: 0;
+  }
+  ::-webkit-scrollbar-thumb {
+    background-color: rgba(4, 160, 22);
+    border-radius: 1.5rem;
+  }
 `;
