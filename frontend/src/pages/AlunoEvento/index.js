56d5b065cd1260397030648f3e03ec0059ddc2aa
Breno Felipe de Bairros <brendo.filipe2050@gmail.com>
2020-10-30 06:58:35 -0300

    Terminado a tela de adicionar um evento

--- frontend/src/pages/AlunoEvento/index.js
+++ frontend/src/pages/AlunoEvento/index.js
@@ -18,7 +18,91 @@ export default function AlunoEvento() {
 
       <Title>Adicionar Atividade</Title>
 
-      <Container></Container>
+      <Container className="container">
+        <form>
+          <div className="form-column raw">
+            <div className="drop-raw">
+              <div className="form-group col-md-6">
+                <label htmlFor="name">Nome da Instituição</label>
+                <input
+                  type="text"
+                  id="name"
+                  className="form-control"
+                  placeholder="Nome da Instituição"
+                />
+              </div>
+              <div className="form-group col-md-6">
+                <label htmlFor="atd">Atividade Complementar</label>
+                <input
+                  type="text"
+                  id="atd"
+                  className="form-control"
+                  placeholder="Atividade Complementar"
+                />
+              </div>
+            </div>
+            <div className="drop-raw">
+              <div className="form-group col-md-6">
+                <label htmlFor="main-text">Modalidade</label>
+                <select id="main-text-2" className="form-control">
+                  <option>Selecione</option>
+                  <option value="">Curso de extenção</option>
+                </select>
+              </div>
+              <div className="form-group col-md-6">
+                <label htmlFor="main-text-2">Atividade</label>
+                <select id="main-text-2" className="form-control">
+                  <option>Selecione</option>
+                  <option value="">Curso de extenção</option>
+                </select>
+              </div>
+            </div>
+            <div className="drop-raw">
+              <div className="form-group col-md-6">
+                <label htmlFor="main-text">Quantidade de Horas</label>
+                <input
+                  type="number"
+                  id="hour"
+                  className="form-control"
+                  placeholder="Suas horas validadas"
+                />
+              </div>
+              <div className="form-group col-md-6">
+                <label htmlFor="hour">Horas Validadas</label>
+                <input
+                  type="number"
+                  id="hour"
+                  className="form-control"
+                  placeholder="Suas horas validadas"
+                />
+              </div>
+            </div>
+            <div className="drop-raw">
+              <div className="form-group col-md-6">
+                <label htmlFor="name">Data de Conclusão</label>
+                <input
+                  type="text"
+                  id="name"
+                  className="form-control"
+                  placeholder="Nome da Instituição"
+                />
+              </div>
+              <div className="form-group col-md-6">
+                <label htmlFor="name">Nome da Instituição</label>
+                <input
+                  type="file"
+                  id="name"
+                  className="form-control"
+                  placeholder="Nome da Instituição"
+                />
+              </div>
+            </div>
+            <div className="btn-salve">
+              <button className="btn btn-primary">Salvar</button>
+            </div>
+          </div>
+        </form>
+      </Container>
     </>
   );
 }
--- frontend/src/pages/AlunoEvento/styled.js
+++ frontend/src/pages/AlunoEvento/styled.js
@@ -1,5 +1,30 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  .raw {
+    display: flex;
+    flex-direction: column;
+  }
 
-export const Title = styled.h2``;
+  .drop-raw {
+    display: flex;
+  }
+
+  .btn-salve {
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+
+    button {
+      width: 500px;
+      border-radius: 6px;
+      margin-top: 9px;
+    }
+  }
+`;
+
+export const Title = styled.h2`
+  text-align: center;
+  margin-top: 50px;
+  margin-bottom: 30px;
+`;
