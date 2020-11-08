5702877055ab4606ac0442b359436d6426771b01
Breno Felipe de Bairros <brendo.filipe2050@gmail.com>
2020-11-08 14:34:47 -0300

    feat: Adicionando o pagina de Consulta das regras

--- frontend/src/pages/AlunoConsultaRegras/index.js
+++ frontend/src/pages/AlunoConsultaRegras/index.js
@@ -1,6 +1,8 @@
 import React, { Fragment } from 'react';
 import Header from '../../components/Header';
+import { Container } from './styled';
 import logo from '../../images/logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
 export default function ConsultaRegras() {
   return (
     <Fragment>
@@ -13,6 +15,44 @@ export default function ConsultaRegras() {
         extencao="Extenção"
         total="Total"
       />
+
+      <Container className="container">
+        <h3 className="text-center">
+          Regras para as Atividades Acadêmicas Complementares
+        </h3>
+        <table className="table">
+          <thead className="bg-success">
+            <tr>
+              <th scope="col" style={{ color: '#fff' }}>
+                Atividade
+              </th>
+              <th scope="col" style={{ color: '#fff' }}>
+                Carga Horaria Equivalente
+              </th>
+              <th scope="col" style={{ color: '#fff' }}>
+                Documentação Exigida
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Aula Inaugural</td>
+              <td>4h</td>
+              <td>Lista de Presença</td>
+            </tr>
+            <tr>
+              <td>Monitoria Acadêmica</td>
+              <td>Máximo 40h</td>
+              <td>Declaração da Instituição</td>
+            </tr>
+            <tr>
+              <td>Visita Técnica</td>
+              <td>4h por visita, máximo 20h</td>
+              <td>Lista de Presença</td>
+            </tr>
+          </tbody>
+        </table>
+      </Container>
     </Fragment>
   );
 }
---
+++ frontend/src/pages/AlunoConsultaRegras/styled.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  h3 {
+    margin-top: 20px;
+    margin-bottom: 40px;
+  }
+`;
