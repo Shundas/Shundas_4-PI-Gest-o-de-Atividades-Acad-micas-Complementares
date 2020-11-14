dfe6ffd364e3a3f1667200c7cb6fd180e088b6cc
Felipe <felipeannunziato04@gmail.com>
2020-11-14 11:16:17 -0300

    Banco de Dados - meia boca

---
+++ backend/knexfile.js
@@ -0,0 +1,21 @@
+// Update with your config settings.
+
+module.exports = {
+
+    development: {
+      client: 'postgresql',
+      connection: {
+        host : '127.0.0.1',
+        user : 'userpi',
+        password : 'shunda2020',
+        database : 'pi',
+        charset: 'utf8'
+      },
+      migrations: {
+        tableName: 'knex_migrations',
+        directory: `${__dirname}/src/database/migrations`
+      }
+    }
+  
+  };
+  
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,3 +2,4 @@ const express = require('./config/express')
 express.listen(3333, () => {
   console.info('Servidor rodando')
 })
+
