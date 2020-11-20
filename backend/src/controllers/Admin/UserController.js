30b92af4512ae8ccc6a885b02270b80ebf1002da
00crono00 <brunozsantos95@gmail.com>
2020-11-19 21:28:03 -0300

    Ajuste update aluno e colaborador

--- backend/src/controllers/Admin/UserController.js
+++ backend/src/controllers/Admin/UserController.js
@@ -103,13 +103,24 @@ module.exports = {
     try {
       const { name, email, phone, celular } = request.body
       const { id } = request.query
-      const validator = yup.object().shape({
-        name: yup.string().required(),
-        email: yup.string().email().required(),
-        celular: yup.string().required(),
-      })
-      if (!(await validator.isValid(request.body))) {
-        return response.status(400).json({ error: 'shunda' })
+      const validatorName = yup.object().shape({ name: yup.string().required() })
+      const validatorEmail = yup.object().shape({ email: yup.string().email().required() })
+      const validatorCelular = yup.object().shape({ celular: yup.string().required() })
+
+      const emailExiste = await knex.count("iduser as existe").from("user").where("email", email)
+      var [{ existe }] = emailExiste
+      if (existe > 0) {
+        return response.status(400).json({ error: 'Email já cadastrado.' })
+      }
+
+      if (!(await validatorName.isValid(request.body))) {
+        return response.status(400).json({ error: 'Nome é campo obrigatório.' })
+      }
+      if (!(await validatorEmail.isValid(request.body))) {
+        return response.status(400).json({ error: 'Email inválido.' })
+      }
+      if (!(await validatorCelular.isValid(request.body))) {
+        return response.status(400).json({ error: 'Celular obrigatório.' })
       }
 
       const updateUser = await knex('user')
@@ -126,13 +137,24 @@ module.exports = {
     try {
       const { name, email, phone, celular } = request.body
       const { id } = request.query
-      const validator = yup.object().shape({
-        name: yup.string().required(),
-        email: yup.string().email().required(),
-        celular: yup.string().required(),
-      })
-      if (!(await validator.isValid(request.body))) {
-        return response.status(400).json({ error: 'shunda' })
+      const validatorName = yup.object().shape({ name: yup.string().required() })
+      const validatorEmail = yup.object().shape({ email: yup.string().email().required() })
+      const validatorCelular = yup.object().shape({ celular: yup.string().required() })
+
+      const emailExiste = await knex.count("iduserSenai as existe").from("userSenai").where("email", email)
+      var [{ existe }] = emailExiste
+      if (existe > 0) {
+        return response.status(400).json({ error: 'Email já cadastrado.' })
+      }
+
+      if (!(await validatorName.isValid(request.body))) {
+        return response.status(400).json({ error: 'Nome é campo obrigatório.' })
+      }
+      if (!(await validatorEmail.isValid(request.body))) {
+        return response.status(400).json({ error: 'Email inválido.' })
+      }
+      if (!(await validatorCelular.isValid(request.body))) {
+        return response.status(400).json({ error: 'Celular obrigatório.' })
       }
 
       const updateUser = await knex('userSenai')
