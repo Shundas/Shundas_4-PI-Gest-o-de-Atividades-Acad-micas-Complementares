d72e11ece339e78ac53947b89f4130be4b1b2ff6
Breno Felipe de Bairros <brendo.filipe2050@gmail.com>
2020-11-21 14:31:10 -0300

    Criando o consulta Colaborador

--- frontend/src/pages/Admin/AdminConsultaColaborador/index.js
+++ frontend/src/pages/Admin/AdminConsultaColaborador/index.js
@@ -3,14 +3,132 @@ import styled from 'styled-components';
 import Header from '../../../components/HeaderAdmin';
 import 'bootstrap/dist/css/bootstrap.css';
 
-const Container = styled.div``;
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 30px;
+  .btns {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    padding-left: 14px;
+    padding-right: 14px;
+  }
+  .cen {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    padding-left: 14px;
+    padding-right: 14px;
+  }
+`;
 
 export default function AdminConsultaColaborador() {
   return (
     <Fragment>
       <Header />
 
-      <Container></Container>
+      <div className="container">
+        <h2 style={{ marginTop: '2em' }} className="text-center">
+          Consulta de Usuário
+        </h2>
+      </div>
+
+      <Container className="container">
+        <form className="form-row">
+          <div className="form-column">
+            <input
+              className="form-control"
+              type="text"
+              id="id1"
+              placeholder="Nome"
+            />
+          </div>
+
+          <div className="cen">
+            <span>{'='}</span>
+          </div>
+
+          <div className="form-column">
+            <input
+              className="form-control"
+              type="text"
+              id="id1"
+              placeholder="Nome"
+            />
+          </div>
+
+          <div className="btns">
+            <button className="btn btn-primary">Filtrar</button>
+          </div>
+        </form>
+      </Container>
+
+      <Container className="container-fluid">
+        <table className="table">
+          <thead className="bg-success">
+            <tr>
+              <th scope="col" style={{ color: '#fff' }}>
+                ID
+              </th>
+              <th scope="col" style={{ color: '#fff' }}>
+                Atividade Complementar
+              </th>
+              <th scope="col" style={{ color: '#fff' }}>
+                Modalidade/Atividade
+              </th>
+              <th scope="col" style={{ color: '#fff' }}>
+                Status
+              </th>
+              <th scope="col" style={{ color: '#fff' }}>
+                Responsavel
+              </th>
+              <th scope="col" style={{ color: '#fff' }}>
+                Ações
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>111</td>
+              <td>Curso de Inglês</td>
+              <td>Ensino</td>
+              <td>Em andamento</td>
+              <td>Jubyscleison</td>
+              <td>
+                <button className="btn btn-outline-success">Vizualizar</button>
+                <button className="btn btn-outline-primary">Editar</button>
+              </td>
+            </tr>
+            <tr>
+              <td>111</td>
+              <td>Curso de Inglês</td>
+              <td>Ensino</td>
+              <td>Em andamento</td>
+              <td>Jubyscleison</td>
+              <td>
+                <button className="btn btn-outline-success">Vizualizar</button>
+                <button className="btn btn-outline-primary">Editar</button>
+              </td>
+            </tr>
+            <tr>
+              <td>111</td>
+              <td>Curso de Inglês</td>
+              <td>Ensino</td>
+              <td>Em andamento</td>
+              <td>Jubyscleison</td>
+              <td>
+                <button className="btn btn-outline-success">Vizualizar</button>
+                <button className="btn btn-outline-primary">Editar</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </Container>
     </Fragment>
   );
 }
