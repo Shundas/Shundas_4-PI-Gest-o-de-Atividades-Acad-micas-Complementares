dba40a57d3049a0e243cd15812271fa689adab36
00crono00 <brunozsantos95@gmail.com>
2020-11-20 22:27:00 -0300

    Mais alguns ajustes de validaçao

--- backend/src/controllers/Aluno/AlunoController.js
+++ backend/src/controllers/Aluno/AlunoController.js
@@ -6,79 +6,78 @@ const crypto = require('crypto')
 module.exports = {
 
     //Alteração de Usuários com validação --OK
-  async updateAluno(request, response) {
-    try {
-      const { name, email, phone, celular } = request.body
-      const { id } = request.query
-      const validatorName = yup.object().shape({ name: yup.string().required() })
-      const validatorEmail = yup.object().shape({ email: yup.string().email().required() })
-      const validatorCelular = yup.object().shape({ celular: yup.string().required() })
+    async updateAluno(request, response) {
+        try {
+            const { name, email, phone, celular } = request.body
+            const { id } = request.query
+            const validatorName = yup.object().shape({ name: yup.string().required() })
+            const validatorEmail = yup.object().shape({ email: yup.string().email().required() })
+            const validatorCelular = yup.object().shape({ celular: yup.string().required() })
 
-      const emailExiste = await knex.count("iduser as existe").from("user").where("email", email)
-      var [{ existe }] = emailExiste
-      if (existe > 0) {
-        return response.status(400).json({ error: 'Email já cadastrado.' })
-      }
 
-      if (!(await validatorName.isValid(request.body))) {
-        return response.status(400).json({ error: 'Nome é campo obrigatório.' })
-      }
-      if (!(await validatorEmail.isValid(request.body))) {
-        return response.status(400).json({ error: 'Email inválido.' })
-      }
-      if (!(await validatorCelular.isValid(request.body))) {
-        return response.status(400).json({ error: 'Celular obrigatório.' })
-      }
 
-      const updateUser = await knex('user')
-        .update({ name: name, email: email, celular: celular, phone: phone })
-        .where('iduser', id)
+            const emailExiste = await knex.count("iduser as existe").from("user").where("email", email)
+            var [{ existe }] = emailExiste
 
-      return response.json(updateUser)
-    } catch (erros) {
-      return response.json({ error: erros.message })
+            if (!(await validatorName.isValid(request.body))) {
+                return response.status(400).json({ error: 'Nome é campo obrigatório.' })
+            }
+            if (!(await validatorEmail.isValid(request.body))) {
+                return response.status(400).json({ error: 'Email inválido.' })
+            }
+            if (!(await validatorCelular.isValid(request.body))) {
+                return response.status(400).json({ error: 'Celular obrigatório.' })
+            }
+
+            const updateUser = await knex('user')
+                .update({ name: name, email: email, celular: celular, phone: phone })
+                .where('iduser', id)
+
+            return response.json(updateUser)
+        } catch (erros) {
+            return response.json({ error: erros.message })
+        }
+    },
+
+    async UpdateSenhaAluno(request, response) {
+        try {
+            const { senhaAtual, novaSenha, confirmaSenha } = request.body
+            const { id } = request.query
+            const senhaBd = await knex
+                .select('senha')
+                .from('user')
+                .where('iduser', id)
+            var [{ senha }] = senhaBd
+            console.log(senha)
+            const result = await bcrypt.compare(senhaAtual, senha)
+            console.log(result)
+
+            if (senhaAtual === novaSenha) {
+                return response
+                    .status(400)
+                    .json({ error: 'Senha atual e nova iguais!' })
+            }
+            if (!result) {
+                return response
+                    .status(400)
+                    .json({ error: 'Senha atual incorreta!' })
+            }
+            if (novaSenha !== confirmaSenha) {
+                return response
+                    .status(400)
+                    .json({ error: 'Senhas não conferem!' })
+            }
+
+            const hash = await bcrypt.hash(novaSenha, 10)
+
+            const updateSenha = await knex('user')
+                .update({ senha: hash })
+                .where('iduser', id)
+
+            return response.json(updateSenha)
+        } catch (erros) {
+            return response.json({ error: erros.message })
+        }
     }
-  },
 
-  async UpdateSenhaAluno(request, response) {
-    try {
-      const { senhaAtual, novaSenha, confirmaSenha } = request.body
-      const { id } = request.query
-      const senhaBd = await knex
-        .select('senha')
-        .from('user')
-        .where('iduser', id)
-      var [{ senha }] = senhaBd
-      console.log(senha)
-      const result = await bcrypt.compare(senhaAtual, senha)
-      console.log(result)
-
-      if (senhaAtual === novaSenha) {
-        return response
-          .status(400)
-          .json({ error: 'Senha atual e nova iguais!' })
-      }
-      if (!result) {
-        return response
-          .status(400)
-          .json({ error: 'Senha atual incorreta!' })
-      }
-      if (novaSenha !== confirmaSenha) {
-        return response
-          .status(400)
-          .json({ error: 'Senhas não conferem!' })
-      }
-
-      const hash = await bcrypt.hash(novaSenha, 10)
-
-      const updateSenha = await knex('user')
-        .update({ senha: hash })
-        .where('iduser', id)
-
-      return response.json(updateSenha)
-    } catch (erros) {
-      return response.json({ error: erros.message })
-    }
-  }
-    
 }
