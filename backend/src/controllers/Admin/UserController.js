becabeb4161f7d0988a9b636352f16a02e2aca10
breno-felipe <brendo.filipe2050@gmail.com>
2020-11-22 08:49:28 -0300

    changes

--- backend/src/controllers/Admin/UserController.js
+++ backend/src/controllers/Admin/UserController.js
@@ -2,25 +2,35 @@ const knex = require('../../database/connection')
 const yup = require('yup')
 const crypto = require('crypto')
 const bcrypt = require('bcryptjs')
-const nodemailer = require('nodemailer');
-const { orWhereRaw, orWhere } = require('../../database/connection');
-
+const nodemailer = require('nodemailer')
+const { orWhereRaw, orWhere } = require('../../database/connection')
 
 module.exports = {
-
   //Criação de Usuários com validação --OK
   async createAluno(request, response, next) {
     try {
       const { name, email, phone, celular, cpf } = request.body
-      const validatorName = yup.object().shape({ name: yup.string().required() })
-      const validatorEmail = yup.object().shape({ email: yup.string().email().required() })
-      const validatorCelular = yup.object().shape({ celular: yup.string().required() })
+      const validatorName = yup
+        .object()
+        .shape({ name: yup.string().required() })
+      const validatorEmail = yup
+        .object()
+        .shape({ email: yup.string().email().required() })
+      const validatorCelular = yup
+        .object()
+        .shape({ celular: yup.string().required() })
       const validatorCpf = yup.object().shape({ cpf: yup.string().required() })
 
-      const emailExiste = await knex.count("iduser as existeEmail").from("user").where("email", email)
+      const emailExiste = await knex
+        .count('iduser as existeEmail')
+        .from('user')
+        .where('email', email)
       var [{ existeEmail }] = emailExiste
 
-      const emailcpf = await knex.count("iduser as existeCpf").from("user").where("cpf", cpf)
+      const emailcpf = await knex
+        .count('iduser as existeCpf')
+        .from('user')
+        .where('cpf', cpf)
       const [{ existeCpf }] = emailcpf
 
       if (existeCpf > 0) {
@@ -43,7 +53,6 @@ module.exports = {
       if (!(await validatorCpf.isValid(request.body))) {
         return response.status(400).json({ error: 'CPF obrigatório.' })
       }
-
 
       const id = crypto.randomBytes(8).toString('hex')
       const senha = crypto.randomBytes(4).toString('hex')
@@ -69,15 +78,27 @@ module.exports = {
   async createColaborador(request, response, next) {
     try {
       const { name, email, phone, celular, idrole, cpf } = request.body
-      const validatorName = yup.object().shape({ name: yup.string().required() })
-      const validatorEmail = yup.object().shape({ email: yup.string().email().required() })
-      const validatorCelular = yup.object().shape({ celular: yup.string().required() })
+      const validatorName = yup
+        .object()
+        .shape({ name: yup.string().required() })
+      const validatorEmail = yup
+        .object()
+        .shape({ email: yup.string().email().required() })
+      const validatorCelular = yup
+        .object()
+        .shape({ celular: yup.string().required() })
       const validatorCpf = yup.object().shape({ cpf: yup.string().required() })
 
-      const emailExiste = await knex.count("iduserSenai as existe").from("userSenai").where("email", email)
+      const emailExiste = await knex
+        .count('iduserSenai as existe')
+        .from('userSenai')
+        .where('email', email)
       var [{ existe }] = emailExiste
 
-      const emailcpf = await knex.count("iduserSenai as existeCpf").from("userSenai").where("cpf", cpf)
+      const emailcpf = await knex
+        .count('iduserSenai as existeCpf')
+        .from('userSenai')
+        .where('cpf', cpf)
       const [{ existeCpf }] = emailcpf
 
       if (!(await validatorName.isValid(request.body))) {
@@ -112,7 +133,7 @@ module.exports = {
         cpf,
         idrole,
         senha: hash,
-        isActive: true
+        isActive: true,
       })
 
       return response.json(user)
@@ -122,42 +143,54 @@ module.exports = {
   },
 
   //Listagem de Usuários --OK
-  async consultaAlunos(request, response){
-    try{
-      const { page =1 } = request.query
+  async consultaAlunos(request, response) {
+    try {
+      const { page = 1 } = request.query
 
-      const [count] = await knex("user").count("*")
-      const alunos = await knex("user")
-      .limit(5)
-      .offset((page -1)*5)
-      .select("iduser","name","email","phone","celular","cpf","isActive")
+      const [count] = await knex('user').count('*')
+      const alunos = await knex('user')
+        .limit(5)
+        .offset((page - 1) * 5)
+        .select(
+          'iduser',
+          'name',
+          'email',
+          'phone',
+          'celular',
+          'cpf',
+          'isActive',
+        )
 
-      response.header("X-Total-Count", count['count(*)'])
+      response.header('X-Total-Count', count['count(*)'])
 
-      return response.json(alunos);
-
-    }catch(erros){
-
+      return response.json(alunos)
+    } catch (erros) {
       return response.json({ error: erros.message })
     }
   },
 
   async consultaColaborador(request, response) {
     try {
+      const { page = 1 } = request.query
 
-      const { page =1 } = request.query
+      const [count] = await knex('userSenai').count('*')
+      const result = await knex('userSenai')
+        .limit(5)
+        .offset((page - 1) * 5)
+        .join('role', 'userSenai.idrole', '=', 'role.idrole')
+        .select(
+          'userSenai.iduserSenai',
+          'role.name',
+          'userSenai.email',
+          'userSenai.phone',
+          'userSenai.celular',
+          'userSenai.cpf',
+          'userSenai.isActive',
+        )
 
-      const [count] = await knex("userSenai").count("*")
-      const result = await knex("userSenai")
-      .limit(5)
-      .offset((page -1)*5)
-      .join('role','userSenai.idrole', '=', 'role.idrole')
-      .select("userSenai.iduserSenai","role.name","userSenai.email","userSenai.phone","userSenai.celular","userSenai.cpf","userSenai.isActive")
+      response.header('X-Total-Count', count['count(*)'])
 
-      response.header("X-Total-Count", count['count(*)'])
-
-      return response.json(result);
-
+      return response.json(result)
     } catch (erros) {
       return response.json({ error: erros.message })
     }
@@ -165,35 +198,47 @@ module.exports = {
 
   async updateAluno(request, response) {
     try {
-        const { name, email, phone, celular, isActive } = request.body
-        const { id } = request.query
-        const validatorName = yup.object().shape({ name: yup.string().required() })
-        const validatorEmail = yup.object().shape({ email: yup.string().email().required() })
-        const validatorCelular = yup.object().shape({ celular: yup.string().required() })
+      const { name, email, phone, celular, isActive } = request.body
+      const { id } = request.query
+      const validatorName = yup
+        .object()
+        .shape({ name: yup.string().required() })
+      const validatorEmail = yup
+        .object()
+        .shape({ email: yup.string().email().required() })
+      const validatorCelular = yup
+        .object()
+        .shape({ celular: yup.string().required() })
 
+      const emailExiste = await knex
+        .count('iduser as existe')
+        .from('user')
+        .where('email', email)
+      var [{ existe }] = emailExiste
 
+      if (!(await validatorName.isValid(request.body))) {
+        return response.status(400).json({ error: 'Nome é campo obrigatório.' })
+      }
+      if (!(await validatorEmail.isValid(request.body))) {
+        return response.status(400).json({ error: 'Email inválido.' })
+      }
+      if (!(await validatorCelular.isValid(request.body))) {
+        return response.status(400).json({ error: 'Celular obrigatório.' })
+      }
 
-        const emailExiste = await knex.count("iduser as existe").from("user").where("email", email)
-        var [{ existe }] = emailExiste
+      const updateUser = await knex('user')
+        .update({
+          name: name,
+          email: email,
+          celular: celular,
+          phone: phone,
+          isActive: isActive,
+        })
+        .where('iduser', id)
 
-        if (!(await validatorName.isValid(request.body))) {
-            return response.status(400).json({ error: 'Nome é campo obrigatório.' })
-        }
-        if (!(await validatorEmail.isValid(request.body))) {
-            return response.status(400).json({ error: 'Email inválido.' })
-        }
-        if (!(await validatorCelular.isValid(request.body))) {
-            return response.status(400).json({ error: 'Celular obrigatório.' })
-        }
-
-        const updateUser = await knex('user')
-            .update({ name: name, email: email, celular: celular, phone: phone, isActive: isActive })
-            .where('iduser', id)
-
-        return response.json(updateUser)
+      return response.json(updateUser)
     } catch (erros) {
-        return response.json({ error: erros.message })
+      return response.json({ error: erros.message })
     }
-}
-
+  },
 }
