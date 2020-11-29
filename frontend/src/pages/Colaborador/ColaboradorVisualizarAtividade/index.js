e4fdab0386e72fa74365789ec4b6c4437f78e30c
breno-felipe <brendo.filipe2050@gmail.com>
2020-11-29 11:34:18 -0300

    Finalizando o tela de Visualizar a atividade

--- frontend/src/pages/Colaborador/ColaboradorVisualizarAtividade/index.js
+++ frontend/src/pages/Colaborador/ColaboradorVisualizarAtividade/index.js
@@ -6,6 +6,7 @@ import Header from '../../../components/HeaderAdmin';
 
 const ContainerApp = styled.div`
   margin-top: 2em;
+  margin-bottom: 3em;
 
   .divider {
     width: 100%;
@@ -43,6 +44,113 @@ export default function PageColaboradorVisualizarAtividade() {
     <>
       <Header />
       <TitleH2>Vizualizar Atividade</TitleH2>
+      <ContainerApp className="container">
+        <form className="form">
+          <SubTitle>Detalhes</SubTitle>
+          <div className="form-column">
+            <div>
+              <div className="form-group col-md-6">
+                <label htmlFor="inst">Nome da Instituição</label>
+                <h3>
+                  <span class="badge badge-success">Senai CTAI</span>
+                </h3>
+              </div>
+
+              <div className="container-divider">
+                <div className="form-group col-md-6">
+                  <label htmlFor="aluno">Nome do Aluno</label>
+                  <h3>
+                    <span class="badge badge-success">
+                      João Boquinha de Veludo
+                    </span>
+                  </h3>
+                </div>
+                <div className="form-group col-md-6">
+                  <label htmlFor="atividade">Atividade Complementar</label>
+                  <h3>
+                    <span class="badge badge-success">
+                      Como ser um Boca de Veludo
+                    </span>
+                  </h3>
+                </div>
+              </div>
+
+              <div className="container-divider">
+                <div className="form-group col-md-6">
+                  <label htmlFor="mod">Modalidade</label>
+                  <h3>
+                    <span class="badge badge-success">Ensino</span>
+                  </h3>
+                </div>
+                <div className="form-group col-md-6">
+                  <label htmlFor="ativ">Atividade</label>
+                  <h3>
+                    <span class="badge badge-success">
+                      Introdução em como ser um Boca de Veludo
+                    </span>
+                  </h3>
+                </div>
+              </div>
+
+              <div className="container-divider">
+                <div className="form-group col-md-6">
+                  <label htmlFor="qtd">Quantidades de Horas</label>
+                  <h3>
+                    <span class="badge badge-success">30h</span>
+                  </h3>
+                </div>
+                <div className="form-group col-md-6">
+                  <label htmlFor="qtd">Horas Validadas</label>
+                  <h3>
+                    <span class="badge badge-success">45h</span>
+                  </h3>
+                </div>
+              </div>
+
+              <div className="container-divider">
+                <div className="form-group col-md-6">
+                  <label htmlFor="date">Data de Conclusão</label>
+                  <h3>
+                    <span class="badge badge-success">20/11/2020</span>
+                  </h3>
+                </div>
+                <div className="form-group col-md-6">
+                  <label htmlFor="fil">Anexo</label>
+                  <h3>
+                    <span class="badge badge-success">Certificado.pdf</span>
+                  </h3>
+                </div>
+              </div>
+              <div className="container-divider">
+                <div className="form-group col-md-6">
+                  <label htmlFor="date">Status</label>
+                  <h3>
+                    <span class="badge badge-warning">Em Andamento</span>
+                  </h3>
+                </div>
+                <div className="form-group col-md-6">
+                  <label htmlFor="fil">Responsavel</label>
+                  <h3>
+                    <span class="badge badge-secondary">Tiago Asp</span>
+                  </h3>
+                </div>
+              </div>
+            </div>
+            <div className="btn-salve">
+              <button className="btn btn-secondary">Editar</button>
+              <button
+                type="button"
+                class="btn btn-primary"
+                data-toggle="modal"
+                data-target="#exampleModalLong"
+              >
+                Enviar comentario
+              </button>
+            </div>
+          </div>
+        </form>
+      </ContainerApp>
+
       <ContainerApp className="container">
         <form className="form">
           <SubTitle>Detalhes</SubTitle>
