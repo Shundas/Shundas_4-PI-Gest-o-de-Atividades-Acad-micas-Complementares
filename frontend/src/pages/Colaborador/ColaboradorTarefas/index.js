fb2e850cead209a2f7c086215d470654c2343b6f
breno-felipe <brendo.filipe2050@gmail.com>
2020-11-29 20:16:26 -0300

    Ajuste

---
+++ frontend/src/pages/Colaborador/ColaboradorTarefas/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+import 'bootstrap/dist/css/bootstrap.css';
+
+import Header from '../../../components/HeaderAdmin';
+
+const ContainerApp = styled.div``;
+
+export default function PageColaboradorTarefas() {
+  return (
+    <>
+      <Header />
+
+      <ContainerApp></ContainerApp>
+    </>
+  );
+}
