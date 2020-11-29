aab4a45ec461a8db510294295caecc633c99b617
breno-felipe <brendo.filipe2050@gmail.com>
2020-11-28 21:50:48 -0300

    Finalizando o header e cards do colaborador

---
+++ frontend/src/components/CardsColaborador/index.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import {
+  FiPlus,
+  FiClipboard,
+  FiTrendingUp,
+  FiCheck,
+  FiSearch,
+} from 'react-icons/fi';
+import 'bootstrap/dist/css/bootstrap.css';
+
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 40,
+  },
+  grid: {
+    display: 'grid',
+    gridTemplateColumns: '1fr 1fr',
+    gap: '30px',
+  },
+
+  center: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    flexWrap: 'wrap',
+  },
+});
+
+export default function CardPageColaborador() {
+  const classes = useStyles();
+
+  return (
+    <>
+      <div className={classes.root}>
+        <ul className={classes.grid}>
+          <div className="card" style={{ width: '18rem' }}>
+            <div className={`card-body ${classes.center}`}>
+              <FiPlus size={25} color="blue" className="icon" />
+              <h5 className="card-title">Adicionar Certificado</h5>
+            </div>
+          </div>
+
+          <div className="card" style={{ width: '18rem' }}>
+            <div className={`card-body ${classes.center}`}>
+              <FiSearch size={25} color="blue" className="icon" />
+              <h5 className="card-title">Consultar Atividades</h5>
+            </div>
+          </div>
+
+          <div className="card" style={{ width: '18rem' }}>
+            <div className={`card-body ${classes.center}`}>
+              <FiClipboard size={25} color="blue" className="icon" />
+              <h5 className="card-title">Consultar Regras</h5>
+            </div>
+          </div>
+
+          <div className="card" style={{ width: '18rem' }}>
+            <div className={`card-body ${classes.center}`}>
+              <FiCheck size={25} color="blue" className="icon" />
+              <h5 className="card-title">Fale Conosco</h5>
+            </div>
+          </div>
+
+          <div className="card" style={{ width: '18rem' }}>
+            <div className={`card-body ${classes.center}`}>
+              <FiTrendingUp size={25} color="blue" className="icon" />
+              <h5 className="card-title">Fale Conosco</h5>
+            </div>
+          </div>
+        </ul>
+      </div>
+    </>
+  );
+}
---
+++ frontend/src/components/HeaderColaborador/index.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { AppBar, Toolbar, Typography, Button } from '@material-ui/core';
+import styled from 'styled-components';
+import { FiUser } from 'react-icons/fi';
+
+import Cards from '../CardsColaborador';
+
+const useStyles = makeStyles(theme => ({
+  appBar: {
+    position: 'static',
+    background: '#28a745',
+    padding: '15px 7px',
+  },
+  toolBar: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  typograph: {
+    marginLeft: '10px',
+    marginRight: '10px',
+  },
+}));
+
+const Background = styled.img`
+  max-width: 208px;
+`;
+
+const ContainerApp = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+`;
+
+export default function AppBarPageColaborador() {
+  const classes = useStyles();
+
+  return (
+    <>
+      <AppBar className={classes.appBar}>
+        <Toolbar className={classes.toolBar}>
+          <ContainerApp>
+            <Background
+              src={require('../../images/logo.svg')}
+              alt="Imagem da Logo"
+            />
+            <Typography className={classes.typograph} variant="h6">
+              Atividades Complementares
+            </Typography>
+          </ContainerApp>
+          <Button color="inherit">
+            <FiUser size={25} />
+          </Button>
+        </Toolbar>
+      </AppBar>
+
+      <Cards />
+    </>
+  );
+}
