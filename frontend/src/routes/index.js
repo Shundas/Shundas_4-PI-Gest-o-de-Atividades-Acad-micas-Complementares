12d1610e1811c1791c82611beb9843d4ab1b1c74
breno-felipe <brendo.filipe2050@gmail.com>
2020-12-05 20:25:18 -0300

    Ajustes

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -11,7 +11,7 @@ import AdminConsultaColborador from '../pages/Admin/AdminConsultaColaborador';
 
 import AlunoEvento from '../pages/Aluno/AlunoEvento';
 
-import ColaboradorHome from '../pages/Colaborador/ColaboradorConsultaProgrsso';
+import ColaboradorHome from '../pages/Colaborador/ColaboradorComentarios';
 
 export default function RoutesAplication() {
   return (
