a322b536f2dcc0b751d3864929fd223eefcd34fa
breno-felipe <brendo.filipe2050@gmail.com>
2020-12-06 13:10:32 -0300

    Iniciando a tela de Evendo do Senai

---
+++ frontend/src/pages/Aluno/AlunoEventoSenai/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+import 'bootstrap/dist/css/bootstrap.css';
+
+import Header from '../../../components/Header';
+
+export default function PageAlunoEventoSenai() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
