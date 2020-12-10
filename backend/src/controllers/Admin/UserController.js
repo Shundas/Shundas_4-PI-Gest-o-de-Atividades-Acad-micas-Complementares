f80d13accbc032c648034e57e1bed4e5ebb9c373
00crono00 <brunozsantos95@gmail.com>
2020-12-09 21:45:11 -0300

    Reset senha colaboradorado

--- backend/src/controllers/Admin/UserController.js
+++ backend/src/controllers/Admin/UserController.js
@@ -358,6 +358,54 @@ module.exports = {
     } catch (error) {
       return response.json({ erro: error.message })
     }
+  },
+
+  async resetSenhaColab(request, response) {
+    try {
+
+      const { email } = request.body
+      const check = await knex.count('iduserSenai as count').from('userSenai').where('email', email).where('isActive', true);
+      var [{ count }] = check
+      console.log(count)
+
+      if (count === 1) {
+
+        const senha = crypto.randomBytes(4).toString('hex')
+        const hash = await bcrypt.hash(senha, 10)
+        const consulta = await knex.select('iduserSenai').from('userSenai').where('email', email);
+        var [{ iduserSenai }] = consulta
+        const updateSenha = await knex('userSenai')
+          .update({ isReset: true, senhaTemp: hash })
+          .where('iduserSenai', iduserSenai)
+
+          // let transport = nodemailer.createTransport({
+          //   host: process.env.APP_HOST,
+          //   port: process.env.APP_PORT,
+          //   secure: false,
+          //   auth: {
+          //     user: process.env.APP_USER,
+          //     pass: process.env.APP_PASS,
+          //   },
+          // })
+    
+          // let info = await transport.sendMail({
+          //   from: '<noreplay@senai.com>',
+          //   to: email,
+          //   subject: `Seu email de redefinição de senha chegou!`,
+          //   text: `Aqui está sua senha provisória para poder redefinir a senha da sua conta: ${senha}`,
+          //   html: `<b>Aqui está sua senha provisória para poder redefinir a senha da sua conta: ${senha}</b>`,
+          // })
+
+        return response.json(updateSenha)
+
+      } else {
+        return response.status(400).json({ error: 'Email inválido/incorreto.' })
+      }
+
+
+    } catch (error) {
+      return response.json({ erro: error.message })
+    }
   }
 
 }
