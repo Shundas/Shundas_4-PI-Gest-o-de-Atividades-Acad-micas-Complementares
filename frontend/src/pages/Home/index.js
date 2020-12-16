a65702eba27f6116eb15694c90680de734130e8b
breno-felipe <brendo.filipe2050@gmail.com>
2020-12-16 19:25:07 -0300

    Finalizando a tela home

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -5,9 +5,29 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 import Header from '../../components/HeaderLogin';
 
-const ContainerApp = styled.div``;
-const Title = styled.h2``;
-const Divider = styled.div``;
+const ContainerApp = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+
+  padding: 70px 0px;
+
+  margin-top: 6em;
+
+  box-shadow: -1px 1px 16px -13px rgba(0, 0, 0, 0.65);
+`;
+const Title = styled.h2`
+  margin-bottom: 1em;
+`;
+const Divider = styled.div`
+  width: 100%;
+  max-width: 320px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
 
 export default function TelaHome() {
   return (
