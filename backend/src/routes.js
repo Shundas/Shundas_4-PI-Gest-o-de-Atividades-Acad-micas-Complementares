accc0d690df69998a4cf9970a63760286790fc68
00crono00 <brunozsantos95@gmail.com>
2020-12-17 01:06:47 -0300

    adlfjsldkf

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -87,6 +87,6 @@ routes.post('/encaminhaCoordenador', AtividadeController.encaminhaCoordenador)
 routes.post('/rejeitaAtividade', AtividadeController.rejeitaAtividade)
 routes.post('/aprovaAtividade', AtividadeController.aprovaAtividade)
 routes.post('/concluiAtividade', AtividadeController.concluiAtividade)
-routes.post('/updateResponsavel', AtividadeController.updateResponsavel)
+routes.put('/updateResponsavel', AtividadeController.updateResponsavel)
 
 module.exports = routes
